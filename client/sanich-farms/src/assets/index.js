9118ef6a717aa493dad4f07acafdd3a674e9b1de
Your Name <your@email.com>
2025-07-26 00:26:48 +0000

    feat: Homepage

--- client/sanich-farms/src/assets/index.js
+++ client/sanich-farms/src/assets/index.js
@@ -1,6 +1,40 @@
 import logo from "./logo.png";
+import image1 from "./image1.png"
+import image2 from "./image2.png"
+import image3 from "./image3.png"
+import image4 from "./image4.png"
+import image5 from "./image5.png"
+import image6 from "./image6.png"
+import image7 from "./image7.png"
+import tsetimage from "./tsetimage.jpg"
+import test2 from "./test2.jpg"
+import feed from "./feed.jpg"
+import wheat from "./wheat.jpg"
+import eggs from "./eggs.jpeg"
+import sevice1 from "./sevice1.jpg";
+import sevice2 from "./sevice2.jpg"
+import sevice3 from "./sevice3.jpg";
+import statistic from "./statistic.jpg"
+
 
 
 export {
-    logo};
+    logo,
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    tsetimage,
+    test2,
+    feed,
+    wheat,
+    eggs,
+    sevice1,
+    sevice2,
+    sevice3,
+    statistic
+};
 
