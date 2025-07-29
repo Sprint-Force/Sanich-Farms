24f932920eff2ab7b85a0c12240a768b3ea46f2a
Virtus Dakura <dakuravirtus@gmail.com>
2025-07-29 03:45:38 +0000

    fix:import into app.jsx

--- client/sanich-farms/src/App.jsx
+++ client/sanich-farms/src/App.jsx
@@ -6,22 +6,6 @@ import Login from './pages/Login/Login';
 import Signup from './pages/Login/Signup';
 import NotFoundPage from './pages/NotFoundPage';
 
-// function App() {
-
-//   return (
-//     <>
-//       <Routes>
-//         {/* Pages Section */}
-//         <Route path='/' element={<Home/>} />
-//         <Route path='/login' element={<Login />} />
-//         <Route path='/signup' element={<Signup />} />
-        
-//         {/* Catch-all route for undefined paths */}
-//         <Route path='*' element={<div className="text-center text-2xl font-bold mt-20">Page Not Found</div>} />
-//       </Routes>
-//     </>
-//   )
-// }
 
 function App() {
   return (
