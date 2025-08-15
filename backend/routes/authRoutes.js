f051e4fa00e4b2a01cbadad5263f82a89ea795f4
Elikplim Yevu <kancartos6@gmail.com>
2025-08-15 19:33:24 +0000

    mod: user profile viewing and updating routes added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,19 @@
 import express from 'express';
-import { forgotPassword, loginUser, registerUser, resetPassword } from '../controllers/UserController.js';
+import { forgotPassword, getProfile, loginUser, registerUser, resetPassword, updateProfile } from '../controllers/UserController.js';
+import { auth } from '../middlewares/auth.js';
 
 
 export const authRoutes = express.Router();
+
+
 
 //Routes
 authRoutes.post('/register', registerUser);
 authRoutes.post('/login', loginUser);
 authRoutes.post('/forgot-password', forgotPassword);
 authRoutes.post('/reset-password', resetPassword);
+
+authRoutes.use(auth);
+// Protected routes
+authRoutes.get('/users/me', getProfile);
+authRoutes.put('/users/me', updateProfile);
